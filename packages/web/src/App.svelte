c27459ca05e36308ba499bb1f3e4763042faa823
AnatoleLucet <lucet.anatole@gmail.com>
2020-04-16 15:38:28 +0200

    init svelte

---
+++ packages/web/src/App.svelte
@@ -0,0 +1,35 @@
+<script type="text/typescript">
+  import Title from "./Title.svelte";
+
+  import { User } from "@packages/api/src/@types/User";
+
+  const user: User = {
+    email: 2
+  };
+
+  export let name: string;
+</script>
+
+<style type="text/scss">
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <Title {name} />
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
---
+++ packages/web/src/Title.svelte
@@ -0,0 +1,16 @@
+<script type="typescript">
+	export let name: string;
+</script>
+
+<h1>Hello {name}!</h1>
+
+<style type="scss">
+	h1 {
+		$color: red;
+
+		color: $color;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
